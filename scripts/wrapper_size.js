448c47dd433d13477692759f539da9387abee00f
reno1se <reno1se.ppv@gmail.com>
2018-07-27 17:11:36 +0300

    12345

--- scripts/wrapper_size.js
+++ scripts/wrapper_size.js
@@ -1,6 +1,5 @@
 var wrapper = document.querySelector("#page_wrapper"),
     width = window.innerWidth,
     height = window.innerHeight;
-
 wrapper.style.width = width;
 wrapper.style.height = height;
